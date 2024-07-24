6afc3d79a7a2950c5525a758bc5907b97b164903
Uboras <129528183+Uboras@users.noreply.github.com>
2024-07-24 20:56:27 -0300

    para mejorar

--- Etiquetas/index.js
+++ Etiquetas/index.js
@@ -1,11 +1,17 @@
 function generateLabel(num) {
+    
+    let cupon = ;
+    let preciodescuento = document.getElementById(`regularPrice${num}`).value - cupon;
+    let regular = document.getElementById(`regularPrice${num}`).value;
+    let numeroDescuento = Math.round((1-(preciodescuento/regular))*100);
+    console.log(numeroDescuento)
     document.getElementById(`labelTitle${num}`).textContent = document.getElementById(`productName${num}`).value;
-    document.getElementById(`labelDiscount${num}`).textContent = document.getElementById(`discount${num}`).value + "%";
+    document.getElementById(`labelDiscount${num}`).textContent = numeroDescuento + "%";
     document.getElementById(`labelPrice${num}`).textContent = '$' + document.getElementById(`price${num}`).value;
-    document.getElementById(`labelValidity${num}`).textContent = document.getElementById(`validity${num}`).value;
+    document.getElementById(`labelValidity${num}`).textContent = "Hasta "+document.getElementById(`validity${num}`).value;
     document.getElementById(`labelMaxUnits${num}`).textContent = 'Máx ' + document.getElementById(`maxUnits${num}`).value + ' u.';
     document.getElementById(`labelRegularPrice${num}`).textContent = '$' + document.getElementById(`regularPrice${num}`).value;
-    document.getElementById(`labelUnitPrice${num}`).textContent = 'PRECIO X 1 L:: ' + document.getElementById(`unitPrice${num}`).value;
+    document.getElementById(`labelUnitPrice${num}`).textContent = 'PRECIO X 1 :: ' + document.getElementById(`price${num}`).value;
     document.getElementById(`labelContainer${num}`).style.display = 'block';
 
     document.getElementById(`form-${num}`).style.display = "none";
@@ -16,7 +22,7 @@ function Eliminar(num){
     document.getElementById(`labelContainer${num}`).style.display = "none";
 }
 function generatePrint() {
-    //document.getElementById(`boton-Impresion`).style.display = "none";
+    
     window.print();
 
 }
