938f7f1f2e31b4d57074fc08774691499fe796aa
Uboras <129528183+Uboras@users.noreply.github.com>
2024-07-24 21:34:03 -0300

    Add files via upload

--- Etiquetas/index.js
+++ Etiquetas/index.js
@@ -1,17 +1,20 @@
 function generateLabel(num) {
-    
-    let cupon = ;
-    let preciodescuento = document.getElementById(`regularPrice${num}`).value - cupon;
+    let titulo = document.getElementById(`productName${num}`).value
+    let cupon = document.getElementById(`price${num}`).value;
+   console.log(cupon);
     let regular = document.getElementById(`regularPrice${num}`).value;
+    let preciodescuento = regular - cupon;
     let numeroDescuento = Math.round((1-(preciodescuento/regular))*100);
-    console.log(numeroDescuento)
-    document.getElementById(`labelTitle${num}`).textContent = document.getElementById(`productName${num}`).value;
+    
+
+
+    document.getElementById(`labelTitle${num}`).textContent = titulo;
     document.getElementById(`labelDiscount${num}`).textContent = numeroDescuento + "%";
-    document.getElementById(`labelPrice${num}`).textContent = '$' + document.getElementById(`price${num}`).value;
+    document.getElementById(`labelPrice${num}`).textContent = '$' + preciodescuento;
     document.getElementById(`labelValidity${num}`).textContent = "Hasta "+document.getElementById(`validity${num}`).value;
-    document.getElementById(`labelMaxUnits${num}`).textContent = 'Máx ' + document.getElementById(`maxUnits${num}`).value + ' u.';
-    document.getElementById(`labelRegularPrice${num}`).textContent = '$' + document.getElementById(`regularPrice${num}`).value;
-    document.getElementById(`labelUnitPrice${num}`).textContent = 'PRECIO X 1 :: ' + document.getElementById(`price${num}`).value;
+    document.getElementById(`labelMaxUnits${num}`).textContent = 'Máx ' + 10+ ' u.';
+    document.getElementById(`labelRegularPrice${num}`).textContent = '$' + regular;
+    document.getElementById(`labelUnitPrice${num}`).textContent = 'PRECIO X 1 :: ' + regular;
     document.getElementById(`labelContainer${num}`).style.display = 'block';
 
     document.getElementById(`form-${num}`).style.display = "none";
