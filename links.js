e2cc96895af269037769c546623e04057600340e
Uboras <129528183+Uboras@users.noreply.github.com>
2024-07-27 21:31:22 -0300

    Responsive

--- links.js
+++ links.js
@@ -2,12 +2,12 @@
    const fondo = "blue"
    const dataMatrix = [
     { content: "https://docs.google.com/forms/d/e/1FAIpQLSfz_CdCLjbi_Sbjh5KVv2a1BqoLLNuQWpc5sKNTTTgshPofCg/viewform", title: "Formulario PFT", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/12Ua3a5JGHZMDenMAa8MCA6Pr7zZ10rRnAJBEue6wL-o/edit", title: "Próximo a vencer. Todos los días", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/1tmRO9C1NIWhbb6rn6nXEwD9yamqHouxg5uetzrP1eNE/edit", title: "Productos bebidas. Sábados a lunes.", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/1YCbIKHxhz2ddark1U4fqWftVHtn75pZW5EtkTa1wIuw/edit", title: "Marca propia: sábados a lunes.", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfY41aHlsXRNVQs_hVqIfLQKzGsZ5B3ckJKRE-G--74ie0-_g/viewform", title: "Productos inactivos.", img: "fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/12Ua3a5JGHZMDenMAa8MCA6Pr7zZ10rRnAJBEue6wL-o/edit", title: "Próximo Vto. Todos los días", img: "fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/1tmRO9C1NIWhbb6rn6nXEwD9yamqHouxg5uetzrP1eNE/edit", title: "Bebidas Sábados a lunes", img: "fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/1YCbIKHxhz2ddark1U4fqWftVHtn75pZW5EtkTa1wIuw/edit", title: "Marca propia: sábados a lunes", img: "fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfY41aHlsXRNVQs_hVqIfLQKzGsZ5B3ckJKRE-G--74ie0-_g/viewform", title: "Productos inactivos", img: "fondo.jfif" },
     { content: "https://docs.google.com/forms/d/e/1FAIpQLSfz_CdCLjbi_Sbjh5KVv2a1BqoLLNuQWpc5sKNTTTgshPofCg/viewform", title: "Panadería y fiambres", img: "fondo.jfif" },
-    { content: "https://forms.gle/jSKSWfE83e1mjD3v9", title: "Planilla 30 60 90.", img: "fondo.jfif" },
+    { content: "https://forms.gle/jSKSWfE83e1mjD3v9", title: "Vto 30-60-90", img: "fondo.jfif" },
     {content: "./Etiquetas/index.html", title: "Etiquetas Liquidación", img: "fondo.jfif"},
     ];
 
