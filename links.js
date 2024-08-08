e43950a84cd3fe88cb1fe24d4a38b7a6179e0883
Uboras <hernanmanera@hotmail.com.ar>
2024-08-08 10:10:20 -0300

    texto y responsive

--- links.js
+++ links.js
@@ -1,14 +1,14 @@
    // Array de enlaces
    const fondo = "blue"
    const dataMatrix = [
-    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfz_CdCLjbi_Sbjh5KVv2a1BqoLLNuQWpc5sKNTTTgshPofCg/viewform", title: "Formulario PFT", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/12Ua3a5JGHZMDenMAa8MCA6Pr7zZ10rRnAJBEue6wL-o/edit", title: "Próximo Vto. Todos los días", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/1tmRO9C1NIWhbb6rn6nXEwD9yamqHouxg5uetzrP1eNE/edit", title: "Bebidas Sábados a lunes", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/1YCbIKHxhz2ddark1U4fqWftVHtn75pZW5EtkTa1wIuw/edit", title: "Marca propia: sábados a lunes", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfY41aHlsXRNVQs_hVqIfLQKzGsZ5B3ckJKRE-G--74ie0-_g/viewform", title: "Productos inactivos", img: "fondo.jfif" },
-    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfz_CdCLjbi_Sbjh5KVv2a1BqoLLNuQWpc5sKNTTTgshPofCg/viewform", title: "Panadería y fiambres", img: "fondo.jfif" },
-    { content: "https://forms.gle/jSKSWfE83e1mjD3v9", title: "Vto 30-60-90", img: "fondo.jfif" },
-    {content: "./Etiquetas/index.html", title: "Etiquetas Liquidación", img: "fondo.jfif"},
+    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfz_CdCLjbi_Sbjh5KVv2a1BqoLLNuQWpc5sKNTTTgshPofCg/viewform", title: "Formulario PFT", img: "./assets/fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/12Ua3a5JGHZMDenMAa8MCA6Pr7zZ10rRnAJBEue6wL-o/edit", title: "Próximo Vto. Todos los días", img: "./assets/fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/1tmRO9C1NIWhbb6rn6nXEwD9yamqHouxg5uetzrP1eNE/edit", title: "Bebidas Sábados a lunes", img: "./assets/fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/1YCbIKHxhz2ddark1U4fqWftVHtn75pZW5EtkTa1wIuw/edit", title: "Marca propia: sábados a lunes", img: "./assets/fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfY41aHlsXRNVQs_hVqIfLQKzGsZ5B3ckJKRE-G--74ie0-_g/viewform", title: "Productos inactivos", img: "./assets/fondo.jfif" },
+    { content: "https://docs.google.com/forms/d/e/1FAIpQLSfz_CdCLjbi_Sbjh5KVv2a1BqoLLNuQWpc5sKNTTTgshPofCg/viewform", title: "Panadería y fiambres", img: "./assets/fondo.jfif" },
+    { content: "https://forms.gle/jSKSWfE83e1mjD3v9", title: "Vto 30-60-90", img: "./assets/fondo.jfif" },
+    {content: "./Etiquetas/index.html", title: "Etiquetas Liquidación", img: "./assets/fondo.jfif"},
     ];
 
 
@@ -28,10 +28,10 @@
            botonElement.addEventListener("click", function () {
                window.location.href = enlace.content;
            });
-           linkDiv.style.backgroundImage = `url(${enlace.img})`;
-           linkDiv.style.backgroundSize = "cover"; 
-           linkDiv.style.backgroundPosition = "center";
-           linkDiv.style.backgroundRepeat = "no-repeat";
+        //    linkDiv.style.backgroundImage = `url(${enlace.img})`;
+        //    linkDiv.style.backgroundSize = "cover"; 
+        //    linkDiv.style.backgroundPosition = "center";
+        //    linkDiv.style.backgroundRepeat = "no-repeat";
            //link.Div.appendChild(svgElement);
            contenedorBotonNombre.appendChild(nombreElement);
            contenedorBotonNombre.appendChild(botonElement);
